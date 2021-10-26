a7d296aa33ee82bc9b2d9b68057d204eeada568e
claresinger <csinger@caltech.edu>
2021-10-25 18:36:18 -0700

    wip example comparing Ruehl and Ovadnevaite models of surface partitioning

---
+++ PySDM_examples/Singer/aerosol.py
@@ -0,0 +1,89 @@
+from pystrict import strict
+from PySDM.physics import spectra, si, constants as const
+from chempy import Substance
+
+compounds = ('(NH4)2SO4', 'betacary')
+
+molar_masses = {
+    "(NH4)2SO4": Substance.from_formula("(NH4)2SO4").mass * si.gram / si.mole,
+    "betacary": 204.36 * si.gram / si.mole
+}
+
+densities = {
+    '(NH4)2SO4': 1.77 * si.g / si.cm**3,
+    'betacary': 0.905 * si.g / si.cm**3
+}
+
+is_organic = {
+    '(NH4)2SO4': False,
+    'betacary': True
+}
+
+ionic_dissociation_phi = {
+    '(NH4)2SO4': 3,
+    'betacary': 1
+}
+
+def volume_fractions(mass_fractions: dict):
+    volume_fractions = {
+        k: (mass_fractions[k] / densities[k]) / sum(
+            mass_fractions[i] / densities[i] for i in compounds
+        ) for k in compounds
+    }
+    return volume_fractions
+
+def f_org_volume(mass_fractions: dict):
+    volfrac = volume_fractions(mass_fractions)
+    return sum(is_organic[k] * volfrac[k] for k in compounds)
+
+def kappa(mass_fractions: dict):
+    kappa = {}
+    for model in ('bulk', 'Ovad', 'Ruehl'):
+        volfrac = volume_fractions(mass_fractions)
+        molar_volumes = {i: molar_masses[i] / densities[i] for i in compounds}
+
+        _masked = {k: (not is_organic[k]) * volfrac[k] for k in compounds}
+        volume_fractions_of_just_inorg = {k:_masked[k] / sum(list(_masked.values())) for k in compounds}
+
+        if model == 'Ovad' or 'Ruehl':
+            ns_per_vol = (1 - f_org_volume(mass_fractions))  * sum(
+                ionic_dissociation_phi[i] * volume_fractions_of_just_inorg[i] / molar_volumes[i] for i in compounds
+            )
+        elif model == 'bulk':
+            ns_per_vol = sum(ionic_dissociation_phi[i] * volfrac[i] / molar_volumes[i] for i in compounds)
+        else:
+            raise AssertionError()
+        kappa[model] = ns_per_vol * const.Mv / const.rho_w
+
+    return kappa
+
+def nu_org(mass_fractions: dict):
+    volfrac = volume_fractions(mass_fractions)
+    molar_volumes = {i: molar_masses[i] / densities[i] for i in compounds}
+
+    _masked = {k: (is_organic[k]) * volfrac[k] for k in compounds}
+    volume_fractions_of_just_org = {k:_masked[k] / sum(list(_masked.values())) for k in compounds}
+    
+    nu_org = sum(volume_fractions_of_just_org[i] * molar_volumes[i] for i in compounds)
+    return nu_org
+
+class _Aerosol:
+    pass
+
+@strict
+class AerosolBetaCary(_Aerosol):
+    def __init__(self, Forg: float = 0.2, N: float = 500):
+        mode = {'(NH4)2SO4': (1-Forg), 'betacary': Forg}
+        self.aerosol_modes_per_cc = (
+        {
+            'f_org': f_org_volume(mode),
+            'kappa': kappa(mode),
+            'nu_org': nu_org(mode),
+            'spectrum': spectra.Lognormal(
+                norm_factor = N / si.cm ** 3,
+                m_mode = 50.0 * si.nm,
+                s_geom = 1.75
+            )
+        }
+    )
+    color = 'dodgerblue'
---
+++ PySDM_examples/Singer/settings.py
@@ -0,0 +1,67 @@
+import numpy as np
+from pystrict import strict
+from PySDM.initialisation import spectral_sampling as spec_sampling
+from PySDM.physics import si, Formulae, constants as const
+from PySDM_examples.Singer.aerosol import _Aerosol
+from PySDM.dynamics.condensation import default_rtol_x, default_rtol_thd
+
+@strict
+class Settings:
+    def __init__(self, dt: float, n_sd_per_mode: int,
+                 aerosol: _Aerosol,
+                 model: str,
+                 spectral_sampling: type(spec_sampling.SpectralSampling),
+                 w: float = 0.32 * si.m / si.s,
+                 rtol_x: float = default_rtol_x,
+                 rtol_thd: float = default_rtol_thd
+                 ):
+        assert model in ('bulk', 'film')
+        self.model = model
+        self.n_sd_per_mode = n_sd_per_mode
+        self.formulae = Formulae(
+            surface_tension='CompressedFilm_Ovadnevaite' if model == 'film' else 'Constant'
+        )
+        self.aerosol = aerosol
+        self.spectral_sampling = spectral_sampling
+        self.t_max = int(210 / w) * si.m
+        self.output_interval = 10 * si.s
+        self.dt = dt
+        self.rtol_x = rtol_x
+        self.rtol_thd = rtol_thd
+
+        self.w = w
+        self.g = 9.81 * si.m / si.s**2
+
+        self.p0 = 980 * si.mbar
+        self.T0 = 280 * si.K
+        pv0 = .99 * self.formulae.saturation_vapour_pressure.pvs_Celsius(self.T0 - const.T0)
+        self.q0 = const.eps * pv0 / (self.p0 - pv0)
+
+        self.cloud_radius_range = (
+                .5 * si.micrometre,
+                np.inf
+        )
+
+        self.mass_of_dry_air = 44
+        
+        self.wet_radius_bins_edges = np.logspace(np.log10(4 * si.um), np.log10(12 * si.um), 128+1, endpoint=True)
+
+    @property
+    def rho0(self):
+        rhod0 = self.formulae.trivia.p_d(self.p0, self.q0) / self.T0 / const.Rd
+        return rhod0 * (1 + self.q0)
+
+    @property
+    def nt(self):
+        nt = self.t_max / self.dt
+        assert nt == int(nt)
+        return int(nt)
+
+    @property
+    def steps_per_output_interval(self) -> int:
+        return int(self.output_interval / self.dt)
+
+    @property
+    def output_steps(self) -> np.ndarray:
+        return np.arange(0, self.nt + 1, self.steps_per_output_interval)
+
