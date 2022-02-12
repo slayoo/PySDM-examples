518004145c2b8f72be982c2d134ea155ef75ff6f
claresinger <clareees@gmail.com>
2022-02-11 21:42:07 -0800

    add pyrcel parcel example from docs with size distribution and supersaturation profile plots

---
+++ PySDM_examples/Pyrcel/aerosol.py
@@ -0,0 +1,104 @@
+from pystrict import strict
+from chempy import Substance
+from PySDM.initialisation import spectra
+from PySDM.physics import si
+from PySDM.physics.constants_defaults import rho_w, Mv
+
+compounds = ('(NH4)2SO4', 'NaCl')
+
+molar_masses = {
+    "(NH4)2SO4": Substance.from_formula("(NH4)2SO4").mass * si.gram / si.mole,
+    "NaCl": Substance.from_formula("NaCl").mass * si.gram / si.mole,
+}
+
+densities = {
+    '(NH4)2SO4': 1.77 * si.g / si.cm**3,
+    'NaCl': 2.16 * si.g / si.cm**3
+}
+
+is_organic = {
+    '(NH4)2SO4': False,
+    'NaCl': False
+}
+
+ionic_dissociation_phi = {
+    '(NH4)2SO4': 3,
+    'NaCl': 2
+}
+
+
+def volume_fractions(mass_fractions: dict):
+    return {
+        k: (mass_fractions[k] / densities[k]) / sum(
+            mass_fractions[i] / densities[i] for i in compounds
+        ) for k in compounds
+    }
+
+
+def f_org_volume(mass_fractions: dict):
+    volfrac = volume_fractions(mass_fractions)
+    return sum(is_organic[k] * volfrac[k] for k in compounds)
+
+
+def kappa(mass_fractions: dict):
+    volfrac = volume_fractions(mass_fractions)
+    molar_volumes = {i: molar_masses[i] / densities[i] for i in compounds}
+
+    _masked = {k: (not is_organic[k]) * volfrac[k] for k in compounds}
+    volume_fractions_of_just_inorg = {
+        k: _masked[k] / sum(list(_masked.values())) for k in compounds
+    }
+    ns_per_vol = sum(ionic_dissociation_phi[i] * volfrac[i] / molar_volumes[i]
+                        for i in compounds)
+    result = ns_per_vol * Mv / rho_w
+
+    return result
+
+
+def nu_org(mass_fractions: dict):
+    volfrac = volume_fractions(mass_fractions)
+    molar_volumes = {i: molar_masses[i] / densities[i] for i in compounds}
+
+    _masked = {k: (is_organic[k]) * volfrac[k] for k in compounds}
+    if sum(list(_masked.values())) == 0:
+        volume_fractions_of_just_org = {k:0.0 for k in compounds}
+    else:
+        volume_fractions_of_just_org = {k:_masked[k] / sum(list(_masked.values())) for k in compounds}
+
+    _nu = sum(volume_fractions_of_just_org[i] * molar_volumes[i] for i in compounds)
+    return _nu
+
+
+class Aerosol:
+    pass
+
+
+@strict
+class AerosolMarine(Aerosol):
+    Aitken = {'(NH4)2SO4': 1.0, 'NaCl': 0}
+    Accumulation = {'(NH4)2SO4': 0, 'NaCl': 1.0}
+
+    def __init__(self):
+        self.aerosol_modes_per_cc = (
+        {
+            'f_org': f_org_volume(self.Aitken),
+            'kappa': kappa(self.Aitken),
+            'nu_org': nu_org(self.Aitken),
+            'spectrum': spectra.Lognormal(
+                norm_factor=850 / si.cm ** 3,
+                m_mode=15 * si.nm,
+                s_geom=1.6
+            )
+        },
+        {
+            'f_org': f_org_volume(self.Accumulation),
+            'kappa': kappa(self.Accumulation),
+            'nu_org': nu_org(self.Accumulation),
+            'spectrum': spectra.Lognormal(
+                norm_factor=10 / si.cm ** 3,
+                m_mode=850 * si.nm,
+                s_geom=1.2
+            ),
+        }
+    )
+    color = 'dodgerblue'
---
+++ PySDM_examples/Pyrcel/simulation.py
@@ -0,0 +1,98 @@
+import numpy as np
+from PySDM.environments import Parcel
+from PySDM import Builder
+from PySDM.backends import CPU
+from PySDM.dynamics import AmbientThermodynamics, Condensation
+from PySDM.initialisation import discretise_multiplicities, equilibrate_wet_radii
+from PySDM.initialisation.spectra import Sum
+import PySDM.products as PySDM_products
+
+
+class Simulation:
+    def __init__(self, settings, products=None):
+        env = Parcel(dt=settings.dt,
+                     mass_of_dry_air=settings.mass_of_dry_air,
+                     p0=settings.p0,
+                     q0=settings.q0,
+                     T0=settings.T0,
+                     w=settings.w)
+        n_sd = settings.n_sd_per_mode * len(settings.aerosol.aerosol_modes_per_cc)
+        builder = Builder(n_sd=n_sd, backend=CPU(formulae=settings.formulae))
+        builder.set_environment(env)
+
+        attributes = {
+            'dry volume':np.empty(0),
+            'dry volume organic':np.empty(0),
+            'kappa times dry volume':np.empty(0),
+            'n': np.ndarray(0)
+        }
+        for mode in settings.aerosol.aerosol_modes_per_cc:
+            r_dry, n_in_dv = settings.spectral_sampling(
+                spectrum=mode['spectrum']).sample(settings.n_sd_per_mode)
+            n_in_dv /= (settings.rho0 / settings.mass_of_dry_air)
+            v_dry = settings.formulae.trivia.volume(radius=r_dry)
+            attributes['n'] = np.append(attributes['n'], n_in_dv)
+            attributes['dry volume'] = np.append(attributes['dry volume'], v_dry)
+            attributes['dry volume organic'] = np.append(
+                attributes['dry volume organic'], mode['f_org'] * v_dry)
+            attributes['kappa times dry volume'] = np.append(
+                attributes['kappa times dry volume'], v_dry * mode['kappa'])
+        for attribute in attributes.values():
+            assert attribute.shape[0] == n_sd
+
+        attributes['n'] = discretise_multiplicities(attributes['n'])
+
+        dv = settings.mass_of_dry_air / settings.rho0
+        # np.testing.assert_approx_equal(
+        #     np.sum(attributes['n']) / dv,
+        #     Sum(tuple(
+        #         settings.aerosol.aerosol_modes_per_cc[i]['spectrum']
+        #         for i in range(len(settings.aerosol.aerosol_modes_per_cc))
+        #     )).norm_factor,
+        #     significant=5
+        # )
+        r_wet = equilibrate_wet_radii(
+            r_dry=settings.formulae.trivia.radius(volume=attributes['dry volume']),
+            environment=env,
+            kappa_times_dry_volume=attributes['kappa times dry volume'],
+            f_org=attributes['dry volume organic'] / attributes['dry volume']
+        )
+        attributes['volume'] = settings.formulae.trivia.volume(radius=r_wet)
+        del attributes['dry volume organic']
+
+        builder.add_dynamic(AmbientThermodynamics())
+        builder.add_dynamic(Condensation())
+
+        products = products or (
+            PySDM_products.ParcelDisplacement(name='z'),
+            PySDM_products.Time(name='t'),
+            PySDM_products.PeakSupersaturation(unit='%', name='S_max'),
+            PySDM_products.ParticleConcentration(name='n_c_cm3', unit='cm^-3',
+                radius_range=settings.cloud_radius_range),
+            PySDM_products.ParticleSizeSpectrumPerVolume(
+                radius_bins_edges=settings.wet_radius_bins_edges),
+        )
+
+        self.particulator = builder.build(attributes=attributes, products=products)
+        self.settings = settings
+
+    def _save_scalars(self, output):
+        for k, v in self.particulator.products.items():
+            if len(v.shape) > 1:
+                continue
+            value = v.get()
+            if isinstance(value, np.ndarray) and value.size == 1:
+                value = value[0]
+            output[k].append(value)
+
+    def _save_spectrum(self, output):
+        value = self.particulator.products['particle size spectrum per volume'].get()
+        output['spectrum'] = value
+
+    def run(self):
+        output = {k: [] for k in self.particulator.products}
+        for step in self.settings.output_steps:
+            self.particulator.run(step - self.particulator.n_steps)
+            self._save_scalars(output)
+        self._save_spectrum(output)
+        return output
