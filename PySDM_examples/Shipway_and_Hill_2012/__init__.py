45855ffcbbc1755400fec99bdc70dd355d0f2718
Emily de Jong <edejong@caltech.edu>
2022-04-14 15:40:34 -0700

    Fix import statemenets for Shipway & deJong

--- PySDM_examples/Shipway_and_Hill_2012/__init__.py
+++ PySDM_examples/Shipway_and_Hill_2012/__init__.py
@@ -2,4 +2,3 @@
 from .plot import plot
 from .settings import Settings
 from .simulation import Simulation
-from .simulation_breakup import SimulationB
--- PySDM_examples/deJong_Mackay_2022/__init__.py
+++ PySDM_examples/deJong_Mackay_2022/__init__.py
@@ -1,4 +1,7 @@
 """
-work-in-progress on deJong & Mackay SDM breakup paper figures
+deJong & Mackay SDM breakup paper figures
 """
 # pylint: disable=invalid-name
+from .settings import Settings
+from .simulation import run_box_breakup, run_box_NObreakup
+from .simulation_breakup import SimulationB
