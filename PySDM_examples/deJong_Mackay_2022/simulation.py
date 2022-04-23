9641cae4d073ea49639b54356a66bfc6ede51025
claresinger <clareees@gmail.com>
2022-04-23 11:32:42 -0700

    bring back make_core for smoke tests

--- PySDM_examples/deJong_Mackay_2022/simulation.py
+++ PySDM_examples/deJong_Mackay_2022/simulation.py
@@ -79,30 +79,30 @@ def run_box_NObreakup(settings, step):
     return (x, y)
 
 
-# def make_core(settings):
-#     backend = CPU
+def make_core(settings):
+    backend = CPU
 
-#     builder = Builder(n_sd=settings.n_sd, backend=backend(settings.formulae))
-#     env = Box(dv=settings.dv, dt=settings.dt)
-#     builder.set_environment(env)
-#     env["rhod"] = 1.0
-#     attributes = {}
-#     attributes["volume"], attributes["n"] = ConstantMultiplicity(
-#         settings.spectrum
-#     ).sample(settings.n_sd)
-#     collision = Collision(
-#         settings.kernel,
-#         settings.coal_eff,
-#         settings.break_eff,
-#         settings.fragmentation,
-#         adaptive=settings.adaptive,
-#     )
-#     builder.add_dynamic(collision)
-#     products = (
-#         ParticleSizeSpectrumPerVolume(
-#             radius_bins_edges=settings.radius_bins_edges, name="dv/dlnr"
-#         ),
-#         CollisionRatePerGridbox(name="cr"),
-#         CollisionRateDeficitPerGridbox(name="crd"),
-#     )
-#     return builder.build(attributes, products)
+    builder = Builder(n_sd=settings.n_sd, backend=backend(settings.formulae))
+    env = Box(dv=settings.dv, dt=settings.dt)
+    builder.set_environment(env)
+    env["rhod"] = 1.0
+    attributes = {}
+    attributes["volume"], attributes["n"] = ConstantMultiplicity(
+        settings.spectrum
+    ).sample(settings.n_sd)
+    collision = Collision(
+        settings.kernel,
+        settings.coal_eff,
+        settings.break_eff,
+        settings.fragmentation,
+        adaptive=settings.adaptive,
+    )
+    builder.add_dynamic(collision)
+    products = (
+        ParticleSizeSpectrumPerVolume(
+            radius_bins_edges=settings.radius_bins_edges, name="dv/dlnr"
+        ),
+        CollisionRatePerGridbox(name="cr"),
+        CollisionRateDeficitPerGridbox(name="crd"),
+    )
+    return builder.build(attributes, products)
