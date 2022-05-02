fb58b5be5b36580e6290f9e6af039af048fe9f83
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-05-02 14:42:37 -0500

    revert Shipway & Hill nb changes; fix pylint comment in deJ_M init

--- PySDM_examples/deJong_Mackay_2022/__init__.py
+++ PySDM_examples/deJong_Mackay_2022/__init__.py
@@ -1,8 +1,7 @@
+# pylint: disable=invalid-name
 """
 deJong & Mackay SDM breakup paper figures
 """
 from .driver1D_with_breakup import SimulationB
-
-# pylint: disable=invalid-name
 from .settings import Settings
 from .simulation import run_box_breakup, run_box_NObreakup
