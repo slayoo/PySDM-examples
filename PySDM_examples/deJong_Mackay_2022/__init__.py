f158fd62afd77fccc79aeb798b8c000ed778efb5
Emily de Jong <edejong@caltech.edu>
2022-05-05 16:26:49 -0700

    Move pylint disable

--- PySDM_examples/deJong_Mackay_2022/__init__.py
+++ PySDM_examples/deJong_Mackay_2022/__init__.py
@@ -1,6 +1,6 @@
-# pylint: disable=invalid-name
 """
 deJong & Mackay SDM breakup paper figures
 """
+# pylint: disable=invalid-name
 from .settings import Settings
 from .simulation import run_box_breakup, run_box_NObreakup
