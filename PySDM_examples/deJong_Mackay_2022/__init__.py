790fda52e97ff3b164dfbbc99d0a501b234c5e30
Emily de Jong <edejong@caltech.edu>
2022-05-05 16:21:55 -0700

    Fix import

--- PySDM_examples/deJong_Mackay_2022/__init__.py
+++ PySDM_examples/deJong_Mackay_2022/__init__.py
@@ -2,6 +2,5 @@
 """
 deJong & Mackay SDM breakup paper figures
 """
-from .driver1D_with_breakup import SimulationB
 from .settings import Settings
 from .simulation import run_box_breakup, run_box_NObreakup
