e6c6ceecb6dffbc889c1366e5632514acee678ac
Sajjad Azimi <azimi@caltech.edu>
2022-05-16 16:08:28 -0700

    removing time sections for saving spectrum and attributes; by default these will be saved as they are time consuming

--- PySDM_examples/Shipway_and_Hill_2012/settings.py
+++ PySDM_examples/Shipway_and_Hill_2012/settings.py
@@ -111,12 +111,7 @@ class Settings:
         )
         self.cloud_water_radius_range = [1 * si.um, 50 * si.um]
         self.rain_water_radius_range = [50 * si.um, np.inf * si.um]
-        self.save_spec_and_attr_times = [
-            15 * si.minutes,
-            20 * si.minutes,
-            25 * si.minutes,
-            30 * si.minutes,
-        ]
+        self.save_spec_and_attr_times = []
 
     @property
     def n_sd(self):
