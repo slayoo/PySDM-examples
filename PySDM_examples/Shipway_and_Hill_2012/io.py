c5ecaad2691233eccd62d86dbf79b9a096484fe3
Sajjad Azimi <azimi@caltech.edu>
2022-05-23 17:55:00 -0700

    vtkexporter_1D works as expected

--- PySDM_examples/Shipway_and_Hill_2012/io.py
+++ PySDM_examples/Shipway_and_Hill_2012/io.py
@@ -2,7 +2,6 @@ import os
 
 import numpy as np
 from pyevtk.hl import pointsToVTK
-from pyevtk.vtk import VtkGroup
 from scipy.io.netcdf import netcdf_file
 
 
@@ -109,12 +108,17 @@ class NetCDFExporter_1D:
 
 class VTKExporter_1D:
     def __init__(
-        self, data, settings, path=".", verbose=False, exclude_particle_reservoir=True
+        self,
+        data,
+        settings,
+        path="./sd_attributes/",
+        verbose=False,
+        exclude_particle_reservoir=True,
     ):
 
-        self.settings = settings
         self.data = data
-        self.path = os.path.join(path, "sd_attributes")
+        self.settings = settings
+        self.path = path
         if not os.path.isdir(self.path):
             os.mkdir(self.path)
 
@@ -122,36 +126,38 @@ class VTKExporter_1D:
         self.exclude_particle_reservoir = exclude_particle_reservoir
 
         self.num_len = len(str(settings.t_max))
-        self.exported_times = {}
 
-    def _write_pvd(self):
-        pvd_attributes = VtkGroup(self.attributes_file_path)
-        for k, v in self.exported_times.items():
-            pvd_attributes.addFile(k + ".vtu", sim_time=v)
-        pvd_attributes.save()
-
-    def _export_attributes(self, time):
-        path = self.attributes_file_path + "_time" + self.add_leading_zeros(time)
-        self.exported_times[path] = time
+    def _export_attributes(self, time_index_and_value):
+        time_index = time_index_and_value[0]
+        time = time_index_and_value[1]
+        path = self.path + "time" + self._add_leading_zeros(time)
 
         if self.verbose:
             print("Exporting Attributes to vtk, path: " + path)
 
         payload = {}
         for k in self.data.keys():
-            payload[k] = self.data[k].to_ndarray(raw=True)
+            if len(self.data[k][time_index].shape) == 1:
+                payload[k] = self.data[k][time_index]
+            elif len(self.data[k][time_index].shape) == 2:
+                assert self.data[k][time_index].shape[0] == 1
+                payload[k] = self.data[k][time_index][0]
+            else:
+                raise NotImplementedError("Shape of data array is not recognized.")
 
-        if len(payload["cell origin"].shape) != 1:
-            raise NotImplementedError("Simulation domain is not 1 dimensional.")
+        z = (
+            self.settings.dz * (payload["cell origin"] + payload["position in cell"])
+            - self.settings.particle_reservoir_depth
+        )
 
         if self.exclude_particle_reservoir:
-            reservoir_particles_indexes = np.where(payload["cell origin"] < 0)
+            reservoir_particles_indexes = np.where(z < 0)
+            z = np.delete(z, reservoir_particles_indexes)
             for k in payload.keys():
                 payload[k] = np.delete(payload[k], reservoir_particles_indexes)
 
-        x = self.settings.dz * (payload["cell origin"] + payload["position in cell"])
-        y = np.full_like(x, 0)
-        z = np.full_like(x, 0)
+        x = np.full_like(z, 0)
+        y = np.full_like(z, 0)
 
         pointsToVTK(path, x, y, z, data=payload)
 
@@ -159,6 +165,5 @@ class VTKExporter_1D:
         return "".join(["0" for i in range(self.num_len - len(str(a)))]) + str(a)
 
     def run(self):
-        for time in self.settings.save_spec_and_attr_times:
-            self._export_attributes(time)
-            self._write_pvd()
+        for time_index_and_value in enumerate(self.settings.save_spec_and_attr_times):
+            self._export_attributes(time_index_and_value)
