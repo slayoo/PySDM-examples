f03c1b2f8ffcafb64165d21a5627868e5b9f0910
edejong-caltech <edejong@caltech.edu>
2022-10-20 11:58:41 -0600

    Clean up & finalize 1D examples

--- PySDM_examples/deJong_Mackay_2022/__init__.py
+++ PySDM_examples/deJong_Mackay_2022/__init__.py
@@ -2,8 +2,6 @@
 deJong & Mackay SDM breakup paper figures
 """
 # pylint: disable=invalid-name
-# from PySDM_examples.Shipway_and_Hill_2012.settings import Settings as Settings1D
-# from PySDM_examples.Shipway_and_Hill_2012.simulation import Simulation as Simulation1D
 from .plot_rates import plot_ax, plot_zeros_ax
 from .settings_0D import Settings0D
 from .simulation_0D import run_box_breakup, run_box_NObreakup
