537b90d396eda53400b655c9f0d2324f537af75b
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-11-09 11:38:40 +0100

    using pcolormesh instead of contourf; log scale for all rates; output_every_n_steps

--- PySDM_examples/deJong_Mackay_2022/plot_rates.py
+++ PySDM_examples/deJong_Mackay_2022/plot_rates.py
@@ -3,6 +3,13 @@ from matplotlib import colors, pyplot
 from PySDM.physics.constants import convert_to, si
 
 
+def log_kwargs(clog, cmin, cmax):
+    if clog:
+        return {"norm": colors.LogNorm(vmin=cmin, vmax=cmax)}
+    else:
+        return {"vmin": cmin, "vmax": cmax}
+
+
 def plot_ax(
     ax,
     var,
@@ -20,30 +27,22 @@ def plot_ax(
     zgrid = output["z"].copy()
     convert_to(zgrid, si.km)
 
-    if cmin is not None and cmax is not None:
-        if clog:
-            levels = np.logspace(np.log10(cmin), np.log10(cmax), 20)
-            data = output[var].copy()
-            data[data == 0] = np.nan
-        else:
-            levels = np.linspace(cmin, cmax, 20)
-            data = output[var]
-
-        mesh = ax.contourf(
-            tgrid,
-            zgrid,
-            data,
-            levels=levels,
-            cmap="BuPu",
-            vmin=cmin,
-            vmax=cmax,
-            extend="max",
-            norm=colors.LogNorm() if clog else None,
-        )
+    if clog:
+        levels = np.logspace(np.log10(cmin), np.log10(cmax), 20)
+        data = output[var].copy()
+        data[data == 0] = np.nan
     else:
-        mesh = ax.contourf(
-            tgrid, zgrid, output[var], levels=20, cmap="BuPu", extend="max"
-        )
+        levels = np.linspace(cmin, cmax, 20)
+        data = output[var]
+
+    mesh = ax.pcolormesh(
+        tgrid,
+        zgrid,
+        data,
+        cmap="BuPu",
+        shading="nearest",
+        **log_kwargs(clog, cmin, cmax),
+    )
 
     if contour_var1 is not None and contour_lvl1 is not None:
         ax.contour(
@@ -72,26 +71,22 @@ def plot_ax(
         cbar_levels = np.logspace(np.log10(cmin), np.log10(cmax), 5, endpoint="True")
     else:
         cbar_levels = np.linspace(cmin, cmax, 5, endpoint="True")
-    cbar = pyplot.colorbar(mesh, fraction=0.05, location="top", ax=ax)
+    cbar = pyplot.colorbar(mesh, fraction=0.05, location="top", extend="max", ax=ax)
     cbar.set_ticks(cbar_levels)
     cbar.set_label(qlabel)
 
 
-def plot_zeros_ax(ax, var, qlabel, output, cmin=None, cmax=None):
+def plot_zeros_ax(ax, var, qlabel, output, cmin=None, cmax=None, clog=False):
     dt = output["t"][1] - output["t"][0]
     dz = output["z"][1] - output["z"][0]
     tgrid = np.concatenate(((output["t"][0] - dt / 2,), output["t"] + dt / 2))
     zgrid = np.concatenate(((output["z"][0] - dz / 2,), output["z"] + dz / 2))
     convert_to(zgrid, si.km)
 
-    # fig = pyplot.figure(constrained_layout=True)
-    output[var + "zero"] = np.zeros_like(output[var])
-    if cmin is not None and cmax is not None:
-        mesh = ax.pcolormesh(
-            tgrid, zgrid, output[var + "zero"], cmap="BuPu", vmin=cmin, vmax=cmax
-        )
-    else:
-        mesh = ax.pcolormesh(tgrid, zgrid, output[var], cmap="BuPu")
+    zeros = np.zeros_like(output[var])
+    kwargs = log_kwargs(clog, cmin, cmax)
+    cmap = "BuPu"
+    mesh = ax.pcolormesh(tgrid, zgrid, zeros, cmap=cmap, **kwargs)
 
     ax.set_xlabel("time [s]")
     ax.set_ylabel("z [km]")
--- PySDM_examples/deJong_Mackay_2022/simulation1D.py
+++ PySDM_examples/deJong_Mackay_2022/simulation1D.py
@@ -1,3 +1,4 @@
+import numpy as np
 import PySDM.products as PySDM_products
 from PySDM.dynamics import Collision
 from PySDM.dynamics.collisions.collision_kernels import Geometric
@@ -6,6 +7,10 @@ from PySDM_examples.Shipway_and_Hill_2012.simulation import Simulation as Simula
 
 
 class Simulation1D(SimulationSH):
+    def __init__(self, settings):
+        super().__init__(settings)
+        self.output_steps = settings.output_steps
+
     @staticmethod
     def add_collision_dynamic(builder, settings, products):
         if settings.breakup:
@@ -31,3 +36,15 @@ class Simulation1D(SimulationSH):
             )
         else:
             SimulationSH.add_collision_dynamic(builder, settings, products)
+
+    def save(self, step):
+        if step in self.output_steps:
+            super().save(step)
+
+    def run(self):
+        result = super().run()
+        for key, val in result.products.items():
+            if len(val.shape) == 2:
+                result.products[key] = val[:, self.output_steps]
+        result.products["t"] = result.products["t"][self.output_steps]
+        return result
