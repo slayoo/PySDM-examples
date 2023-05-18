358f614f856dbca860c6da8ec1a785fa11edeaa2
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2023-05-18 18:34:49 +0200

    phase out t-t-r.txt; remove some of the files that will become obsolete after repo merging

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 import platform
 
 from setuptools import find_packages, setup
@@ -8,6 +9,8 @@ def get_long_description():
         long_description = file.read()
     return long_description
 
+
+CI = "CI" in os.environ
 
 setup(
     name="PySDM-examples",
@@ -27,6 +30,29 @@ setup(
         "ghapi",
     ]
     + (["pyvinecopulib", "vtk"] if platform.architecture()[0] != "32bit" else []),
+    extras_require={
+        "tests": [
+            "pytest",
+            "nbconvert",
+            "jupyter-core" + "<5.0.0" if CI else "",
+            "jsonschema" + "==3.2.0"
+            if CI
+            else "",  # https://github.com/jupyter/nbformat/issues/232
+            "Jinja2" + "<3.0.0"
+            if CI
+            else "",  # https://github.com/jupyter/nbconvert/issues/1568
+            "MarkupSafe" + "<2.1.0"
+            if CI
+            else "",  # https://github.com/aws/aws-sam-cli/issues/3661
+            "Pillow" + "<9.1.0"
+            if CI
+            else "",  # https://github.com/python-pillow/Pillow/blob/0606f02860d0c4449bc047a6187043b6188a7404/docs/releasenotes/9.1.0.rst#deprecations
+            "ipywidgets" + "<8.0.3" if CI else "",
+            "ipykernel" + "<6.22.0"
+            if CI
+            else "",  # https://github.com/dask/distributed/issues/7688
+        ]
+    },
     author="https://github.com/open-atmos/PySDM/graphs/contributors",
     author_email="slayoo@staszic.waw.pl",
     long_description=get_long_description(),
