d503018a82406e9ef1fba6952450f9ace81d603d
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2023-05-18 21:35:24 +0200

    fix typo

--- PySDM_examples/Szumowski_et_al_1998/gui_settings.py
+++ PySDM_examples/Szumowski_et_al_1998/gui_settings.py
@@ -309,28 +309,28 @@ class GUISettings:
         for widget in self.ui_mpdata_options:
             if widget.description == self.tot_description:
                 return widget.value
-        raise NotImplemetedError()
+        raise NotImplementedError()
 
     @property
     def mpdata_fct(self):
         for widget in self.ui_mpdata_options:
             if widget.description == self.fct_description:
                 return widget.value
-        raise NotImplemetedError()
+        raise NotImplementedError()
 
     @property
     def mpdata_iga(self):
         for widget in self.ui_mpdata_options:
             if widget.description == self.iga_description:
                 return widget.value
-        raise NotImplemetedError()
+        raise NotImplementedError()
 
     @property
     def mpdata_iters(self):
         for widget in self.ui_mpdata_options:
             if widget.description == self.nit_description:
                 return widget.value
-        raise NotImplemetedError()
+        raise NotImplementedError()
 
     def stream_function(self, xX, zZ, _):
         assert hasattr(self.__settings, "rhod_w_max")
